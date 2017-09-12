4f7352e2c8e653fdd1528edb1dcb3c4a6b79d495
Rachel Ah Chuen <rachel@rachelMBP15.local>
2017-09-12 11:27:34 -0700

    description

--- deepmodel.py
+++ deepmodel.py
@@ -1,7 +1,8 @@
 """
 N-layer neural network 
-Input -> (N-1) layers [z_l/Relu] -> 1 layer [z_n/Sigmoid] -> Output
+Input -> (N-1) layers [z_l/Relu] -> 1 layer [z_N/Sigmoid] -> Output
 where z_l = W_l*a_(l-1) + b_l for layer l
+Loss function = cross entropy
 """
 import numpy as np
 import matplotlib.pyplot as plt
